c97ffa9ef55f54993b5722f92aaa45a35918f5fc
Christopher Wong <christopherpk1996@gmail.com>
2020-04-05 19:21:58 +0800

    Optimize build scripts

--- client/scripts/postbuild.js
+++ client/scripts/postbuild.js
@@ -1,8 +1,5 @@
 const shell = require("shelljs");
 
 shell.exec("cd ../");
-shell.rm("-rf", "../build/client");
-shell.mkdir("-p", "../build/client");
-shell.exec("react-scripts build");
 shell.mv("build/*", "../build/client");
 shell.rm("-rf", "build");
--- server/scripts/prebuild.js
+++ server/scripts/prebuild.js
@@ -3,5 +3,3 @@ const shell = require("shelljs");
 shell.exec("cd ../");
 shell.rm("-rf", "../build/server");
 shell.mkdir("-p", "../build/server");
-shell.exec("babel -d \"../build/server\" --ignore node_modules . -s");
-shell.cp("-rf", "node_modules", "../build/server");
